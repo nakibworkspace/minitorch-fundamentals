2c2fc357b9f2e374bd958d99269735514cca82a2
nakibworkspace <rajnakib2003@gmail.com>
2026-03-03 11:12:10 +0000

    3.2 testing

--- minitorch/tensor_functions.py
+++ minitorch/tensor_functions.py
@@ -269,21 +269,31 @@ class MatMul(Function):
     @staticmethod
     def forward(ctx: Context, t1: Tensor, t2: Tensor) -> Tensor:
         ctx.save_for_backward(t1, t2)
+        # Use the fast matrix multiply defined in fast_ops.py
         return t1.f.matrix_multiply(t1, t2)
 
     @staticmethod
     def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, Tensor]:
         t1, t2 = ctx.saved_values
+        
+        # We need to flip the last two dims for the gradient math
+        # dL/dA = dL/dC @ B.T
+        # dL/dB = A.T @ dC
+        
+        t1_T = t1.transpose(len(t1.shape) - 2, len(t1.shape) - 1)
+        t2_T = t2.transpose(len(t2.shape) - 2, len(t2.shape) - 1)
 
-        def transpose(a: Tensor) -> Tensor:
-            order = list(range(a.dims))
-            order[-2], order[-1] = order[-1], order[-2]
-            return a._new(a._tensor.permute(*order))
+        grad_t1 = grad_output.f.matrix_multiply(grad_output, t2_T)
+        grad_t2 = grad_output.f.matrix_multiply(t1_T, grad_output)
 
-        return (
-            grad_output.f.matrix_multiply(grad_output, transpose(t2)),
-            grad_output.f.matrix_multiply(transpose(t1), grad_output),
-        )
+        # CRITICAL: If t1 was (1, 4, 3) and grad_output was (5, 4, 2)
+        # grad_t1 will now be (5, 4, 3). We must sum back to (1, 4, 3).
+        if t1.shape[0] == 1 and grad_t1.shape[0] > 1:
+            grad_t1 = grad_t1.sum(0)
+        if t2.shape[0] == 1 and grad_t2.shape[0] > 1:
+            grad_t2 = grad_t2.sum(0)
+
+        return grad_t1, grad_t2
 
 
 # Helpers for Constructing tensors
